45a5809dc0e408d35c96026b10490c3d89623a86
Jhow17 <silvajonathan234@gmail.com>
2026-09-08 23:38:00 -0300

    feat: cria classe abstrata FuncionarioTransporte

---
+++ CP1/FuncionarioTransporte.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CP1
+{
+    public abstract class FuncionarioTransporte
+    {
+        private string nome;
+        private int registro;
+
+        public string Nome {
+            get { return nome; }
+
+            set { nome = value; }
+
+        }
+
+
+
+
+    }
+}
